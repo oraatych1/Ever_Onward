74de142c092b342f5f2bc2cb0e487443cb183953
J <jmbischer49765@yahoo.com>
2021-03-30 00:47:05 -0400

    Expanded GreyBox + UI
    added more to greybox and added ui with some functionality

--- Ever_Onward/Assets/Scripts/HealthAndManaSystem.cs
+++ Ever_Onward/Assets/Scripts/HealthAndManaSystem.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthAndManaSystem : MonoBehaviour
 {
 
     public float health { get; private set; }
     public float healthMax = 100;
+    public Scrollbar healthbar;
 
     public float mana { get; private set; }
     public float manaMax = 100;
+    public Scrollbar manabar;
 
     private void Start()
     {
         health = healthMax;
         mana = manaMax;
     }
-
+    public void Update()
+    {
+        healthbar.size = health/100;
+        manabar.size = mana/100;
+    }
     public void UseMana(float amt)
     {
 

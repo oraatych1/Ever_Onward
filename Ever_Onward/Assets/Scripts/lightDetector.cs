120865f1655d942f66aed18fe0000c19f149a3c3
Danin <tenerid@ferris.edu>
2021-12-04 21:29:07 -0500

    Light Detector Script started
    Base code for Light Detection has been added, will continue working on it

---
+++ Ever_Onward/Assets/Scripts/lightDetector.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightDetector : MonoBehaviour
+{
+    Animator anim;
+    Renderer rend;
+    Collider m_Collider;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //rend = GetComponent<Renderer>();
+        m_Collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Grass")
+        {
+            anim.SetBool("isWilted", false);
+            //rend.material.color = Color.red;
+            Invoke("SetAddCollision", 2f);
+            Invoke("DisableCollision", 15f);
+        }
+    }
+
+    void SetAddCollision()
+    {
+        m_Collider.enabled = true;
+    }
+
+    void DisableCollision()
+    {
+        anim.SetBool("isWilted", true);
+        //rend.material.color = Color.gray;
+        m_Collider.enabled = false;
+    }
+}
